3ec14cbf954d114baeefbe4887b0d4c06be8c123
PETERSON SIBILIM DE ANDRADE <peterson.sandrade@senacsp.edu.br>
2025-12-04 15:39:56 -0300

    att

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener('scroll', () => {
+    let menuCheck = document.getElementById('check')
+    //se a caixa check 
+    if (menuCheck.checked){
+        menuCheck.checked = false
+    }
+}) 
